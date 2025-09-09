43b65a92d54edcd74a8d0995203d3d4c7cfcd0a1
Erik-TheCreator <erikdesousagoncalves1009@gmail.com>
2025-09-08 21:45:21 -0300

    fix(PagePrincipal.jsx) Corrigindo bugs de autententicação

--- GameTracker/src/pages/PagePrincipal.jsx
+++ GameTracker/src/pages/PagePrincipal.jsx
@@ -1,22 +1,22 @@
 import "./PagePrincipal.css"
-import { MdOutlineSearch } from "react-icons/md";
-import { MdAddToPhotos } from "react-icons/md";
+import { MdOutlineSearch,MdAddToPhotos } from "react-icons/md";
 import { useState,useEffect } from "react";
 
 export const PagePrincipal = () => {
     const [games,setGames]= useState([]);
     const [nome, setNome]= useState("");
 
-    useEffect(()=> {
-    fetch("http://localhost:3000/gametracker"),{
-    credentials: "include"}
+useEffect(() => {
+  fetch("http://localhost:3000/gametracker", {
+    credentials: "include"
+  })
     .then(res => {
-      if (!res.ok) throw new Error("Erro no http de games")
-      return res.json()
+      if (!res.ok) throw new Error("Erro no http de games");
+      return res.json();
     })
-    .then(date => setGames(date))
-    .catch(erro => console.log(erro))
-  }, []);
+    .then(data => setGames(data))
+    .catch(erro => console.log("Erro no fetch:", erro));
+}, []);
 
   const gamesFiltrados = games.filter(game =>
   game.titulo.toLowerCase().includes(nome.toLowerCase())
@@ -29,7 +29,7 @@ export const PagePrincipal = () => {
             <div className="containerGames">
                 <form>
                     <div className="searchBar">
-                    <input type="text" value={nome} onChange={(e)=> setNome(e.target.value)} placeholder="Pesquisar"/>
+                    <input type="text" value={nome} onChange={(e)=> setNome(e.target.value)}/>
                     <MdOutlineSearch className="searchIcon"/>
                     </div>
                     <select name="" id="">Ano de Lançamento</select>
@@ -37,9 +37,9 @@ export const PagePrincipal = () => {
 
 
                 </form>
-                <div className="gamesRow">
 
-                    {gamesFiltrados.map((game)=>(
+                <div className="gamesRow">
+                     {gamesFiltrados.map((game)=>(
                         <div className="games"  style={{    backgroundImage: `url(/imagens/${game.capa})`,
                           }}>
                             <div className="game-title"><span>{game.titulo}</span><MdAddToPhotos className="addicon"/>
@@ -47,6 +47,9 @@ export const PagePrincipal = () => {
                         </div>
 
                     ))}
+
+             
+             </div>
                    
                     
 
@@ -55,7 +58,7 @@ export const PagePrincipal = () => {
             </div>
 
 
-        </div>
+   
         
   )
 }
